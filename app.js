aefb805605f07d7d14b5290c88bc09683d4518e5
mykke <1micheal.sihavong2@gmail.com>
2017-08-24 06:08:23 +0000

    complete show and create routes. also sort mangoDB for index route

--- app.js
+++ app.js
@@ -25,20 +25,20 @@ var blogSchema = new mongoose.Schema({
 var Blog = mongoose.model('Blog', blogSchema);
 
 // Blog.create({
-//     title: "Rock&ROLL!!",
-//     image: "http://www.semprefamilia.com.br/wp-content/uploads/2015/10/School-of-Rock-620x765.jpg",
+//     title: "CHANGE WORLD",
+//     image: "https://unsplash.com/?photo=_hpk_92Crhs",
 //     body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
 // })
 
-// RESTful Routing
-    // Root route
+// RESTful ROUTING
+    // ROOT ROUTE
     app.get(['/', '/home'], function(req, res){
         res.redirect("/blogs");
     });
     
-    // Index route
+    // INDEX ROUTE
     app.get('/blogs', function(req, res){
-        Blog.find({}, function(err, blogs){
+        Blog.find().sort('-created').exec(function(err, blogs){
            if (err){
                console.log(err);
            } else {
@@ -47,12 +47,12 @@ var Blog = mongoose.model('Blog', blogSchema);
         });
     });
     
-    // New route - show create form
+    // NEW ROUTE - show create form
     app.get('/blogs/new', function(req, res){
         res.render('new');
     });
     
-    // Create route
+    // CREATE ROUTE
     app.post('/blogs', function(req, res){
         // sanitize body to wrap HTML with protection lol
         req.body.blog.body = req.sanitize(req.body.blog.body);
@@ -65,6 +65,18 @@ var Blog = mongoose.model('Blog', blogSchema);
         });
     });
     
+    // SHOW ROUTE
+    app.get('/blogs/:id', function(req, res){
+        Blog.findById(req.params.id, function(err, foundBlog){
+            if (err){
+                console.log(err);
+            } else {
+                res.render('show', {blog:foundBlog});
+            }
+        });
+    });
+
+
 app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server is running!");
 })
